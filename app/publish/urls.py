a576b9a946ee84d7e8d003859a86058bc71108fb
esojay5000 <esojay5000@yahoo.com>
2023-01-14 22:04:47 -0330

    Added Publish App

---
+++ app/publish/urls.py
@@ -0,0 +1,9 @@
+"""
+URL mapping for the publish app.
+"""
+from django.urls import path
+from publish import views
+
+urlpatterns = [
+    path('', views.index, name='index')
+]
--- app/publish/views.py
+++ app/publish/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """Render the ladning page for the publish page."""
+    return render(request, 'publish/index.html')
